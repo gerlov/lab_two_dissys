c5dae08ce36ef96926437e619fb6958b1b475a7e
Freezy727 <107752697+Freezy727@users.noreply.github.com>
2024-10-10 15:55:37 +0200

    some commit?

--- AuctionApp/obj/Debug/net8.0/AuctionApp.AssemblyInfo.cs
+++ AuctionApp/obj/Debug/net8.0/AuctionApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+737df89be2c529b2cf73fc296d3d96697c7fcd4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af22908dc4fb9c973e77a607b190cbae004d8d34")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
